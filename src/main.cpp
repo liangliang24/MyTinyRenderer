2104e30f3c0f125aa6404908671f6949ca561809
LLTouchingFish <62233176+liangliang24@users.noreply.github.com>
2025-03-15 17:32:49 +0800

    Lesson2: Triangle rasterization and back face culling

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <vector>
 #include <cmath>
+#include <random>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
 
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor red	 = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 Model* model = nullptr;
 int width = 800;
-int height = 600;
+int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage& tgaImage, const TGAColor& tgaColor)
 {
@@ -47,6 +49,48 @@ void line(int x0, int y0, int x1, int y1, TGAImage& tgaImage, const TGAColor& tg
 	}
 }
 
+void line(Vec2i v0, Vec2i v1, TGAImage& tgaImage, const TGAColor& tgaColor)
+{
+	line(v0.x, v0.y, v1.x, v1.y, tgaImage, tgaColor);
+}
+
+Vec3f barycentric(Vec2i* pts, Vec2i P) {
+	Vec3f u = Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]) ^ Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]);
+	/* `pts` and `P` has integer value as coordinates
+	   so `abs(u[2])` < 1 means `u[2]` is 0, that means
+	   triangle is degenerate, in this case return something with negative coordinates */
+	if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1);
+	return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+}
+
+void triangle(Vec2i* pts, TGAImage& image, TGAColor color)
+{
+	Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+	Vec2i bboxmax(0, 0);
+	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+	for (int i = 0; i < 3; i++) {
+		bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+		bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+		bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+		bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+	}
+	Vec2i P;
+	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+			Vec3f bc_screen = barycentric(pts, P);
+			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+			image.set(P.x, P.y, color);
+		}
+	}	
+}
+
+void triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage& image, TGAColor color)
+{
+	Vec2i pts[3] = { v0, v1, v2 };
+	triangle(pts, image, color);
+}
+
 int main(int argc, char** argv) {
 	if (argc == 2)
 	{
@@ -58,16 +102,23 @@ int main(int argc, char** argv) {
 	}
 
 	TGAImage image(width, height, TGAImage::RGB);
+
+	Vec3f light_dir(0, 0, -1); // define light_dir
+
 	for (int i = 0; i < model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
+		Vec2i screen_coords[3];
+		Vec3f world_coords[3];
 		for (int j = 0; j < 3; j++) {
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j + 1) % 3]);
-			int x0 = (v0.x + 1.) * width / 2.;
-			int y0 = (v0.y + 1.) * height / 2.;
-			int x1 = (v1.x + 1.) * width / 2.;
-			int y1 = (v1.y + 1.) * height / 2.;
-			line(x0, y0, x1, y1, image, white);
+			Vec3f v = model->vert(face[j]);
+			screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+			world_coords[j] = v;
+		}
+		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+		n.normalize();
+		float intensity = n * light_dir;
+		if (intensity > 0) {
+			triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
 		}
 	}
 
