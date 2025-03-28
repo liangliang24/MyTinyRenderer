bc10e22c387633a976f92b138160247fd7768f56
LLTouchingFish <62233176+liangliang24@users.noreply.github.com>
2025-03-28 21:47:27 +0800

    Code Maintenance
    1.优化项目结构
    2.用cmake替换premake
    3.替换原有数学库，使用glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <vector>
 #include <cmath>
 #include <random>
+#include <glm/vec2.hpp>
+#include <glm/geometric.hpp>
 #include "tgaimage.h"
 #include "model.h"
-#include "geometry.h"
 
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
@@ -50,30 +51,30 @@ void line(int x0, int y0, int x1, int y1, TGAImage& tgaImage, const TGAColor& tg
 	}
 }
 
-void line(Vec2i v0, Vec2i v1, TGAImage& tgaImage, const TGAColor& tgaColor)
+void line(glm::vec2 v0, glm::vec2 v1, TGAImage& tgaImage, const TGAColor& tgaColor)
 {
 	line(v0.x, v0.y, v1.x, v1.y, tgaImage, tgaColor);
 }
 
-Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P)
+glm::vec3 barycentric(glm::vec3 A, glm::vec3 B, glm::vec3 C, glm::vec3 P)
 {
-	Vec3f s[2];
+	glm::vec3 s[2];
 	for (int i = 1; i >= 0; i--) {
 		s[i][0] = C[i] - A[i];
 		s[i][1] = B[i] - A[i];
 		s[i][2] = A[i] - P[i];
 	}
-	Vec3f u = cross(s[0], s[1]);
+	glm::vec3 u = cross(s[0], s[1]);
 	if (std::abs(u[2]) > 1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
-		return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
-	return Vec3f(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
+		return glm::vec3(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+	return glm::vec3(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
 }
 
-void triangle(Vec3f* pts, TGAImage& image, TGAColor color)
+void triangle(glm::vec3* pts, TGAImage& image, TGAColor color)
 {
-	Vec2f bboxmin(image.get_width() - 1, image.get_height() - 1);
-	Vec2f bboxmax(0, 0);
-	Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
+	glm::vec2 bboxmin(image.get_width() - 1, image.get_height() - 1);
+	glm::vec2 bboxmax(0, 0);
+	glm::vec2 clamp(image.get_width() - 1, image.get_height() - 1);
 	for (int i = 0; i < 3; i++) {
 		bboxmin.x = std::max(0.f, std::min(bboxmin.x, pts[i].x));
 		bboxmin.y = std::max(0.f, std::min(bboxmin.y, pts[i].y));
@@ -81,10 +82,10 @@ void triangle(Vec3f* pts, TGAImage& image, TGAColor color)
 		bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
 		bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
 	}
-	Vec3f P;
+	glm::vec3 P;
 	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
 		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
-			Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
+			glm::vec3 bc_screen = barycentric(pts[0], pts[1], pts[2], P);
 			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
 			P.z = 0;
 			for (int i = 0; i < 3; i++) P.z += pts[i][2] * bc_screen[i];
@@ -96,15 +97,15 @@ void triangle(Vec3f* pts, TGAImage& image, TGAColor color)
 	}	
 }
 
-void triangle(Vec3f v0, Vec3f v1, Vec3f v2, TGAImage& image, TGAColor color)
+void triangle(glm::vec3 v0, glm::vec3 v1, glm::vec3 v2, TGAImage& image, TGAColor color)
 {
-	Vec3f pts[3] = { v0, v1, v2 };
+	glm::vec3 pts[3] = { v0, v1, v2 };
 	triangle(pts, image, color);
 }
 
-Vec3f world2screen(Vec3f v) 
+glm::vec3 world2screen(glm::vec3 v)
 {
-	return Vec3f(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
+	return glm::vec3(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
 }
 
 int main(int argc, char** argv) {
@@ -114,27 +115,26 @@ int main(int argc, char** argv) {
 	}
 	else
 	{
-		model = new Model("res/obj/african_head.obj");
+		model = new Model("../res/obj/african_head.obj");
 	}
 
 	TGAImage image(width, height, TGAImage::RGB);
 
 	zbuffer = new float[width * height];
 	for (int i = width * height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
-	Vec3f light_dir(0, 0, -1); // define light_dir
+	glm::vec3 light_dir(0, 0, -1); // define light_dir
 
 	for (int i = 0; i < model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
-		Vec3f screen_coords[3];
-		Vec3f world_coords[3];
+		glm::vec3 screen_coords[3];
+		glm::vec3 world_coords[3];
 		for (int j = 0; j < 3; j++) {
-			Vec3f v = model->vert(face[j]);
+			glm::vec3 v = model->vert(face[j]);
 			screen_coords[j] = world2screen(v);
 			world_coords[j] = v;
 		}
-		Vec3f n = cross((world_coords[2] - world_coords[0]) , (world_coords[1] - world_coords[0]));
-		n.normalize();
-		float intensity = n * light_dir;
+		glm::vec3 n = glm::normalize(glm::cross((world_coords[2] - world_coords[0]) , (world_coords[1] - world_coords[0])));
+		float intensity = glm::dot(n, light_dir);
 		triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
 	}
 
